92c2ec35c75709f34b021a895d6c23dc94dc425c
duelpasta <lambrechts.jasper@gmail.com>
2018-04-18 23:47:45 +0200

    .

--- src/controller/GerberParser.java
+++ src/controller/GerberParser.java
@@ -3,10 +3,21 @@ package controller;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class GerberParser {
 
     private Scanner scan;
+    private static final Pattern REGEX_RECT = Pattern.compile("<tag>(.+?)</tag>");
+    private static final Pattern REGEX_CIRC = Pattern.compile("<tag>(.+?)</tag>");
+    private static final Pattern REGEX_OBLONG = Pattern.compile("<tag>(.+?)</tag>");
+    private static final Pattern REGEX_POLYGON = Pattern.compile("<tag>(.+?)</tag>");
+    private static final Pattern REGEX_MACRO = Pattern.compile("<tag>(.+?)</tag>");
+    private static final Pattern REGEX_FLASHES = Pattern.compile("D03");
+    private Pattern pattRect;
+
+
     public GerberParser() {
         System.out.println("File Received");
     }
@@ -20,7 +31,12 @@ public class GerberParser {
         scan.useDelimiter(",");
 
         while (scan.hasNext()) {
-            System.out.println(scan.next());
+            String line = scan.next();
+            parseRect(line);
         }
+    }
+
+    private void parseRect(String line) {
+        Matcher matcher = REGEX_RECT.matcher(line);
     }
 }
