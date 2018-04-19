778cfb30384ce739f297b11f813569c2088670da
Jasper Lambrects <lambrechts.jasper@gmail.com>
2018-04-19 17:06:02 +0200

    Shape parser REGEX working

--- src/controller/GerberParser.java
+++ src/controller/GerberParser.java
@@ -9,13 +9,10 @@ import java.util.regex.Pattern;
 public class GerberParser {
 
     private Scanner scan;
-    private static final Pattern REGEX_RECT = Pattern.compile("<tag>(.+?)</tag>");
-    private static final Pattern REGEX_CIRC = Pattern.compile("<tag>(.+?)</tag>");
-    private static final Pattern REGEX_OBLONG = Pattern.compile("<tag>(.+?)</tag>");
-    private static final Pattern REGEX_POLYGON = Pattern.compile("<tag>(.+?)</tag>");
-    private static final Pattern REGEX_MACRO = Pattern.compile("<tag>(.+?)</tag>");
+    private static final Pattern REGEX_FIND_APERTURES = Pattern.compile("[%ADD](\\d*)(R|C|O),(\\d*[.]\\d*)*X?(\\d[.]\\d*)?");
     private static final Pattern REGEX_FLASHES = Pattern.compile("D03");
     private Pattern pattRect;
+    private Matcher matcher;
 
 
     public GerberParser() {
@@ -28,7 +25,6 @@ public class GerberParser {
         catch (FileNotFoundException e) {e.printStackTrace();}
 
         System.out.println("Loaded the following file and start parsing " + file);
-        scan.useDelimiter(",");
 
         while (scan.hasNext()) {
             String line = scan.next();
@@ -37,6 +33,9 @@ public class GerberParser {
     }
 
     private void parseRect(String line) {
-        Matcher matcher = REGEX_RECT.matcher(line);
+                Matcher matcher = REGEX_FIND_APERTURES.matcher(line);
+                if (matcher.find()) {
+                    System.out.println("D-code: " + matcher.group(1) + "\t" + "Shape:" + matcher.group(2) + "\t\t" + "X: " + matcher.group(3) + "\t\t" + "Y: " + matcher.group(4) );
+                }
     }
 }
