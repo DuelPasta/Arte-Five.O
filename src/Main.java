6c6ad952c0ab39e2c2559b86a31e927074f9784c
Jasper Lambrechts <lambrechts.jasper@gmail.com>
2018-05-08 23:46:33 +0200

    .Replaced buggy JFilechooesr with more recent javaFFX file chooser

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,33 @@
 import controller.Parser;
+import javafx.application.Application;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 import javax.swing.*;
 import java.io.File;
 import java.util.Scanner;
 
-public class Main {
+public class Main extends Application {
 
     private static double thickness;
 
-    public static void main(String[] args) {
-        Initialize();
+    public void start(Stage primaryStage) {
+        FileChooser configFileChooser = new FileChooser();
+
+        File configFile = configFileChooser.showOpenDialog(null);
+        getThickness();
+        Parser parser = new Parser(configFile, thickness);
+        parser.parse();
+        System.exit(1);
     }
 
-    private static void Initialize() {
-        //TODO Find better alternative for JFilechooser as it's buggy.
-        File file;
-        JFileChooser chooser = new JFileChooser();
-        int result = chooser.showOpenDialog(null);
-        if (result == JFileChooser.APPROVE_OPTION) {
-            file = chooser.getSelectedFile();
-            getThickness();
-            Parser parser = new Parser(file, thickness);
-            parser.parse();
-        }  else {
-            JOptionPane.showMessageDialog(null, "No file selected");
-            System.exit(1);
-        }
+    public static void main(String[] args) {
+        launch(args);
     }
 
     private static void getThickness() {
         Scanner scan = new Scanner(System.in);
         System.out.println("Please enter thickness:");
-        thickness = scan.nextDouble()/1000;
+        thickness = scan.nextDouble() / 1000;
     }
 }
